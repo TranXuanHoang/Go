5ba04d6d970ae6f29b5b560f49c473a87842ba71
TranXuanHoang <hoangtx.ict@gmail.com>
2020-11-01 23:29:31 +0900

    Update persons data when clicking a button

--- 00-basics/src/App.js
+++ 00-basics/src/App.js
@@ -11,10 +11,21 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Tran Hoang', age: 30 },
+        { name: 'Bob', age: 26 },
+        { name: 'Christopher', age: 32 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello, World!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
