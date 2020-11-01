1092f05a92c4bc4cb7df0378dfc7eea08315063f
TranXuanHoang <hoangtx.ict@gmail.com>
2020-11-01 14:26:45 +0900

    Define person component and use it in app component

--- 00-basics/src/App.js
+++ 00-basics/src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hoang', age: 29 },
+      { name: 'Bob', age: 26 },
+      { name: 'Chris', age: 31 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello, World!</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
