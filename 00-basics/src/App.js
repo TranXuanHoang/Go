aafe9550576fa7eb149c28b99efc830697ade385
TranXuanHoang <hoangtx.ict@gmail.com>
2020-11-04 12:00:30 +0900

    Remove styled-components and use CSS modules for CSS styling

--- 00-basics/src/App.js
+++ 00-basics/src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import './App.css'
+import classes from './App.css'
 import Person from './Person/Person'
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`
 
 class App extends Component {
   state = {
@@ -55,6 +40,7 @@ class App extends Component {
 
   render() {
     let persons = null
+    let btnClass = ''
 
     if (this.state.showPersons) {
       persons = (
@@ -69,25 +55,26 @@ class App extends Component {
           })}
         </div>
       )
+
+      btnClass = classes.Red
     }
 
-    const classes = []
+    const assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hello, World!</h1>
-        <p className={classes.join(' ')}>Learning React...</p>
-        <StyledButton
-          alt={this.state.showPersons}
+        <p className={assignedClasses.join(' ')}>Learning React...</p>
+        <button className={btnClass}
           onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     )
