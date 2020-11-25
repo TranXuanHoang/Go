cdf936d1bc15ac148daba277a58999b8e86a259b
TranXuanHoang <hoangtx.ict@gmail.com>
2020-11-25 22:18:03 +0900

    Add an orders page
    * Define an orders component which represents the orders page
    * Define an order component that is each order in the orders list
    * Revise the NavigationItems and NavigationItem to use React Router to navigate between pages
    * Write logic to navigate to and show orders on the orders page
    * Update readme file

--- 01-burgers/src/App.js
+++ 01-burgers/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Layout>
         <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
       </Layout>
