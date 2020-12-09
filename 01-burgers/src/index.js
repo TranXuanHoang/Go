d2458d36e597f937e4b1ad10a5fc03bce213b9c9
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-09 23:22:54 +0900

    Connect Redux Devtools to the Redux store

--- 01-burgers/src/index.js
+++ 01-burgers/src/index.js
@@ -8,7 +8,10 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import reducer from './store/reducer';
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <React.StrictMode>
