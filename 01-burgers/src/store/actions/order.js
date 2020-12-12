6ce9af6b163e4a0e54ed5459d01f8313f5c68f39
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-12 22:39:34 +0900

    Fetch orders via Redux
    * Handle asynchronous code for fetching orders with Redux Thunk

--- 01-burgers/src/store/actions/order.js
+++ 01-burgers/src/store/actions/order.js
@@ -40,3 +40,42 @@ export const purchaseInit = () => {
     type: actionTypes.PURCHASE_INIT
   }
 }
+
+export const fetchORdersSuccess = orders => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  }
+}
+export const fetchORdersFail = error => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  }
+}
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  }
+}
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart())
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = []
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          })
+        }
+        dispatch(fetchORdersSuccess(fetchedOrders))
+      })
+      .catch(err => {
+        dispatch(fetchORdersFail(err))
+      })
+  }
+}
