46c1ee9916a6470c53ff2cb25672537cf64eab01
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-23 02:52:49 +0900

    Handle auto sign-in with a Redux Saga
    * Move side-effect async code of checking authentication status to Redux Saga

--- 01-burgers/src/store/sagas/auth.js
+++ 01-burgers/src/store/sagas/auth.js
@@ -48,3 +48,23 @@ export function* authUserSaga(action) {
     yield put(actions.authFail(error.response.data.error))
   }
 }
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem('token')
+  if (!token) {
+    yield put(actions.logout())
+  } else {
+    const expirationDate = yield new Date(localStorage.getItem('expirationDate'))
+    if (expirationDate <= new Date()) {
+      // Token has already expired - Logout
+      yield put(actions.logout())
+    } else {
+      // Token hasn't expired yet - Login
+      const userId = yield localStorage.getItem('userId')
+      yield put(actions.authSuccess(token, userId))
+      yield put(actions.checkAuthTimeout(
+        (expirationDate.getTime() - new Date().getTime()) / 1000
+      ))
+    }
+  }
+}
