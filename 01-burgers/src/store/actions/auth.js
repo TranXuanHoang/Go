87c7c286517979bc09d40b42bd73d8f738a3b4e6
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-23 01:11:09 +0900

    Handle user authentication with Redux Saga
    * Move async code of user authentication from the Redux action creator to a Redux saga

--- 01-burgers/src/store/actions/auth.js
+++ 01-burgers/src/store/actions/auth.js
@@ -1,10 +1,4 @@
-import axios from 'axios'
 import * as actionTypes from './actionTypes'
-
-// For more information about Firebase Auth REST API,
-// see https://firebase.google.com/docs/reference/rest/auth
-const GOOGLE_AUTH_API_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts'
-const GOOGLE_API_KEY = 'AIzaSyDgsUotZEJIHSUnv26Vcv9gslbpbxXeiNw'
 
 export const authStart = () => {
   return {
@@ -51,29 +45,11 @@ export const checkAuthTimeout = (exprirationTime) => {
 }
 
 export const auth = (email, password, isSignup) => {
-  return dispatch => {
-    dispatch(authStart())
-    const authData = {
-      email,
-      password,
-      returnSecureToken: true
-    }
-    let url = `${GOOGLE_AUTH_API_BASE_URL}:signUp?key=${GOOGLE_API_KEY}`
-    if (!isSignup) {
-      url = `${GOOGLE_AUTH_API_BASE_URL}:signInWithPassword?key=${GOOGLE_API_KEY}`
-    }
-    axios.post(url, authData)
-      .then(response => {
-        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
-        localStorage.setItem('token', response.data.idToken)
-        localStorage.setItem('expirationDate', expirationDate)
-        localStorage.setItem('userId', response.data.localId)
-        dispatch(authSuccess(response.data.idToken, response.data.localId))
-        dispatch(checkAuthTimeout(response.data.expiresIn))
-      })
-      .catch(err => {
-        dispatch(authFail(err.response.data.error))
-      })
+  return {
+    type: actionTypes.AUTH_USER,
+    email,
+    password,
+    isSignup
   }
 }
 
--- 01-burgers/src/store/sagas/auth.js
+++ 01-burgers/src/store/sagas/auth.js
@@ -1,5 +1,11 @@
+import axios from 'axios'
 import { delay, put } from 'redux-saga/effects'
 import * as actions from '../actions/index'
+
+// For more information about Firebase Auth REST API,
+// see https://firebase.google.com/docs/reference/rest/auth
+const GOOGLE_AUTH_API_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts'
+const GOOGLE_API_KEY = 'AIzaSyDgsUotZEJIHSUnv26Vcv9gslbpbxXeiNw'
 
 /**
  * The asterisk '*' after the 'function' keyword turns 'logout' to be
@@ -16,4 +22,29 @@ export function* logoutSaga(action) {
 export function* checkAuthTimeoutSaga(action) {
   yield delay(action.exprirationTime * 1000)
   yield put(actions.logout())
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart())
+  const authData = {
+    email: action.email,
+    password: action.password,
+    returnSecureToken: true
+  }
+  let url = `${GOOGLE_AUTH_API_BASE_URL}:signUp?key=${GOOGLE_API_KEY}`
+  if (!action.isSignup) {
+    url = `${GOOGLE_AUTH_API_BASE_URL}:signInWithPassword?key=${GOOGLE_API_KEY}`
+  }
+
+  try {
+    const response = yield axios.post(url, authData)
+    const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000)
+    yield localStorage.setItem('token', response.data.idToken)
+    yield localStorage.setItem('expirationDate', expirationDate)
+    yield localStorage.setItem('userId', response.data.localId)
+    yield put(actions.authSuccess(response.data.idToken, response.data.localId))
+    yield put(actions.checkAuthTimeout(response.data.expiresIn))
+  } catch (error) {
+    yield put(actions.authFail(error.response.data.error))
+  }
 }
