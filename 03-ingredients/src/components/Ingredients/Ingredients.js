bcb8bac0b540cc23c5bfbe927d1debe6401eb09a
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-24 23:56:10 +0900

    Store ingredients data to a backend database
    * Save newly added ingredient data to a Firebase Realtime database

--- 03-ingredients/src/components/Ingredients/Ingredients.js
+++ 03-ingredients/src/components/Ingredients/Ingredients.js
@@ -3,14 +3,26 @@ import IngredientForm from "./IngredientForm";
 import IngredientList from './IngredientList';
 import Search from "./Search";
 
+const FIREBASE_REALTIME_DB = 'https://react-ingredients-3feb6-default-rtdb.firebaseio.com/'
+
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([])
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ])
+    fetch(`${FIREBASE_REALTIME_DB}ingredients.json`, {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ])
+      })
   }
 
   const removeIngredientHandler = id => {
