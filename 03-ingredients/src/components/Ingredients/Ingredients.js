795e4f01ac6ce52a5a884da9491575eba3d37551
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-26 01:40:20 +0900

    Manage complex state logic with useReducer hook
    * Change from using useState to useReducer hook to manage multiple sub-states (ingredients, loading, error)
    * The commented code is the old implementation in which useState was used to create and manage these sub-states

--- 03-ingredients/src/components/Ingredients/Ingredients.js
+++ 03-ingredients/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useReducer } from "react";
 import ErrorModal from '../UI/ErrorModal';
 import IngredientForm from "./IngredientForm";
 import IngredientList from './IngredientList';
@@ -6,18 +6,50 @@ import Search from "./Search";
 
 const FIREBASE_REALTIME_DB = 'https://react-ingredients-3feb6-default-rtdb.firebaseio.com/'
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Should not get there!')
+  }
+}
+
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null }
+    case 'RESPONSE':
+      return { ...curHttpState, loading: false }
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage }
+    case 'CLEAR':
+      return { ...curHttpState, error: null }
+    default:
+      throw new Error('Should not be reached!')
+  }
+}
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState()
+  // const [userIngredients, setUserIngredients] = useState([])
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [error, setError] = useState()
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, dispatchHttp] = useReducer(httpReducer, { loading: false, error: null })
 
   const filterIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients)
+    // setUserIngredients(filteredIngredients)
+    dispatch({ type: 'SET', ingredients: filteredIngredients })
   }, [])
 
   const addIngredientHandler = ingredient => {
-    setIsLoading(true)
-    fetch(`${FIREBASE_REALTIME_DB}ingredients.json`, {
+    // setIsLoading(true)
+    dispatchHttp({ type: 'SEND' })
+    fetch(`${FIREBASE_REALTIME_DB}ingredients.jsons`, {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: {
@@ -25,44 +57,55 @@ const Ingredients = () => {
       }
     })
       .then(response => {
-        setIsLoading(false)
+        // setIsLoading(false)
+        dispatchHttp({ type: 'RESPONSE' })
         return response.json()
       })
       .then(responseData => {
-        setUserIngredients(prevIngredients => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient }
-        ])
+        // setUserIngredients(prevIngredients => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient }
+        // ])
+        dispatch({
+          type: 'ADD',
+          ingredient: { id: responseData.name, ...ingredient }
+        })
       })
       .catch(error => {
-        setError('Something went wrong!')
-        setIsLoading(false)
+        // setError('Something went wrong!')
+        // setIsLoading(false)
+        dispatchHttp({ type: 'ERROR', errorMessage: 'Something went wrong!' })
       })
   }
 
   const removeIngredientHandler = id => {
-    setIsLoading(true)
-    fetch(`${FIREBASE_REALTIME_DB}ingredients/${id}.json`, {
+    // setIsLoading(true)
+    dispatchHttp({ type: 'SEND' })
+    fetch(`${FIREBASE_REALTIME_DB}ingredients/${id}.jsons`, {
       method: 'DELETE'
     })
       .then(response => {
-        setIsLoading(false)
-        setUserIngredients(prevIngredients => prevIngredients.filter(ig => ig.id !== id))
+        // setIsLoading(false)
+        // setUserIngredients(prevIngredients => prevIngredients.filter(ig => ig.id !== id))
+        dispatchHttp({ type: 'RESPONSE' })
+        dispatch({ type: 'DELETE', id: id })
       })
       .catch(error => {
-        setError('Something went wrong!')
-        setIsLoading(false)
+        // setError('Something went wrong!')
+        // setIsLoading(false)
+        dispatchHttp({ type: 'ERROR', errorMessage: 'Something went wrong!' })
       })
   }
 
   const clearError = () => {
-    setError(null)
+    // setError(null)
+    dispatchHttp({ type: 'CLEAR' })
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading} />
 
       <section>
         <Search onLoadIngredients={filterIngredientsHandler} />
