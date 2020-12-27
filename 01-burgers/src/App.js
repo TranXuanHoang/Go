147dde9012d2b08dabbbc1af12cf6892fed28e98
TranXuanHoang <hoangtx.ict@gmail.com>
2020-12-27 22:18:30 +0900

    Convert the App class-based component to functional component with React Hooks

--- 01-burgers/src/App.js
+++ 01-burgers/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 // import Auth from './containers/Auth/Auth';
@@ -14,42 +14,40 @@ const Logout = lazy(() => import('./containers/Auth/Logout/Logout'))
 const Checkout = lazy(() => import('./containers/Checkout/Checkout'))
 const Orders = lazy(() => import('./containers/Orders/Orders'))
 
-export class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup()
-  }
+const App = props => {
+  useEffect(() => {
+    props.onTryAutoSignup()
+  }, [props])
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  )
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
         <Route path="/auth" component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
     )
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/" />
-        </Switch>
-      )
-    }
-
-    return (
-      <div>
-        <Layout>
-          <Suspense fallback={<div>Loading...</div>}>
-            {routes}
-          </Suspense>
-        </Layout>
-      </div>
-    )
   }
+
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<div>Loading...</div>}>
+          {routes}
+        </Suspense>
+      </Layout>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
