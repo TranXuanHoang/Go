e07306b237f3f7f1106626e7cf1ceb027b3c89dd
TranXuanHoang <hoangtx.ict@gmail.com>
2021-02-01 00:50:11 +0900

    Implement multi-tier navigation
    * Synchronize routing between the container and marketing microfrontend
    * Use browser router for the container, in-memory router for child microfrontend(s)

--- 06-mfp/packages/container/src/components/MarketingApp.js
+++ 06-mfp/packages/container/src/components/MarketingApp.js
@@ -1,12 +1,27 @@
 import { mount } from 'marketing/MarketingApp'
 import React, { useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default () => {
   const ref = useRef(null)
 
+  // Get currently being used route history object
+  // that is the browser route history
+  const history = useHistory()
+
   useEffect(() => {
-    mount(ref.current)
-  })
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      }
+    })
+
+    history.listen(onParentNavigate)
+  }, [])
 
   return <div ref={ref} />
 }
--- 06-mfp/packages/marketing/src/bootstrap.js
+++ 06-mfp/packages/marketing/src/bootstrap.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
-// Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el)
+/**
+ * Mount function to start up the app
+ * @param {HTMLElement} el the HTML DOM element to which this `marketing`
+ * microfrontend is rendered.
+ * @param {object} object containing
+ * - `onNavigate()` function that is passed down from the `container` to change
+ * its route according to the route of this child `marketing` microfrontend.
+ * - `defaultHistory` that is a `MemoryHistory` route object used a default router
+ * setting while running this `marketing` microfrontend in development mode.
+ * @return {object} an object contains `onParentNavigate()` function
+ * that is passed up to the `container` to listen for route changing event there
+ * and update the in-memory route of the this child `marketing` microfrontend.
+ */
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if (onNavigate) {
+    // Listen to route changing event of the marketing microfrontend
+    // If event occurred, update browser route of the container with
+    // the onNavigate callback function
+    history.listen(onNavigate)
+  }
+
+  ReactDOM.render(<App history={history} />, el)
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const pathname = history.location
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
 }
 
 
@@ -14,7 +46,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root')
 
   if (devRoot) {
-    mount(devRoot)
+    mount(devRoot, { defaultHistory: createBrowserHistory() })
   }
 }
 
